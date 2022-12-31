5d1f19dccb11a5c29f6f8d838cec83095971fe5c
Abdulaziz20007 <92323615+Abdulaziz20007@users.noreply.github.com>
2022-12-31 20:31:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b4f378a932msh39e3a3465e0289ap166efejsn9d26a0dbfbed',
+		'X-RapidAPI-Host': 'muslimsalat.p.rapidapi.com'
+	}
+};
+let city = 'tashkent'
+function tas() {
+	city = 'tashkent'
+	open()
+}
+function and() {
+	city = 'andijan'
+	open()
+}
+function nam() {
+	city = 'namangan'
+	open()
+}
+function far() {
+	city = 'fergana'
+	open()
+}
+function kok() {
+	city = 'kokand'
+	open()
+}
+function bux() {
+	city = 'bukhara'
+	open()
+}
+function nav() {
+	city = 'navoiy'
+	open()
+}
+function sam() {
+	city = 'samarkand'
+	open()
+}
+function sir() {
+	city = 'sirdaryo'
+	open()
+}
+function qas() {
+	city = 'qarshi'
+	open()
+}
+function sur() {
+	city = 'andijan'
+	open()
+}
+function xor() {
+	city = 'andijan'
+	open()
+}
+function open() {
+	fetch('https://muslimsalat.p.rapidapi.com/'+city+'.json', options)
+	.then(response => response.json())
+	.then(response => {console.log(response),
+		document.getElementById('b-time').innerHTML = response.items[0].fajr
+		document.getElementById('p-time').innerHTML = response.items[0].dhuhr
+		document.getElementById('a-time').innerHTML = response.items[0].asr
+		document.getElementById('s-time').innerHTML = response.items[0].maghrib
+		document.getElementById('x-time').innerHTML = response.items[0].isha
+	})
+}
